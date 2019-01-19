76eedee0d86595afaf4e65243417fca7b24b582a
Carlos Fontes <fontessystems@gmail.com>
2019-01-19 16:19:04 -0400

    se crean archivos para el login y register

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-// Ruta inicial
+// Home
 router.get("/", (req, res) => {
   res.render("home", { title: 'home' });
+});
+
+// Login
+router.get("/login", (req, res) => {
+  res.render("login", { title: 'login' });
+});
+
+// Register
+router.get("/register", (req, res) => {
+  res.render("register", { title: 'register' });
 });
 
 module.exports = router;
