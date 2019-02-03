db0a19eeb6efb4db1fede44f95b9c71f788ebb24
Carlos Fontes <fontessystems@gmail.com>
2019-02-03 00:06:31 -0400

    cambios en los nombres de los modelos airplane y aircraft

--- start.js
+++ start.js
@@ -14,14 +14,16 @@ sequelize.authenticate()
 
 // Declaramos los modelos que tendrá nuestra Base de Datos
 const Employee = require('./models/Employee');
-const Aircraft = require('./models/Aircraft');
+const Airplane = require('./models/AirplaneModel');
+const AirplaneModel = require('./models/AirplaneModel');
 const Provider = require('./models/Provider');
 const Customer = require('./models/Customer');
 
 
 
 // Declaramos todas las relaciones entre nuestros modelos
-Airplane.hasMany(Aircraft, {foreignKey: 'Model'});
+// Airplane.hasMany(Aircraft, {foreignKey: 'model'});
+AirplaneModel.hasMany(Airplane);
 
 
 // Le decimos a sequelize que cree las tablas
